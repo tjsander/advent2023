049bf2a166cc37709f3551c7a21fad038d37955a
Travis Sanders <tjsander@gmail.com>
2023-12-01 14:32:12 -0800

    Star 2 down. Woof. Got to be a quicker dirtier way to do this.

--- day1/day1.py
+++ day1/day1.py
@@ -2,16 +2,46 @@
 import re
 
 # INPUT = 'day1/test_input.txt'
+# INPUT = 'day1/test_input2.txt'
 INPUT = 'day1/input.txt'
 
-# On each line, the calibration value can be found by 
-# combining the first digit and the last digit (in that order) 
+NUMSTRINGS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+# On each line, the calibration value can be found by
+# combining the first digit and the last digit (in that order)
 # to form a single two-digit number.
 
-
-def remove_item(list, item): 
-    res = [i for i in list if i != item] 
+def remove_item(list, item):
+    res = [i for i in list if i != item]
     return res
+
+
+def numstring_to_digit(line):
+    line = line + ""
+    # print (line)
+
+    loopbreak = 0
+    for x in range(0, len(line)):
+        if (loopbreak == 1): break
+        if (line[x].isdigit()): break
+        for y in range(0, 10):
+            if (line[x:len(NUMSTRINGS[y])+x] == NUMSTRINGS[y]):
+                print (line + " found " + NUMSTRINGS[y])
+                line = line.replace(NUMSTRINGS[y], str(y), 1)
+                loopbreak = 1
+                break
+
+    loopbreak = 0
+    for x in range(0, len(line)):
+        if (loopbreak == 1): break
+        z = len(line) - x
+        for y in range(0, 10):
+            if (line[z-len(NUMSTRINGS[y]):z] == NUMSTRINGS[y]):
+                print (line + " found " + NUMSTRINGS[y])
+                line = line.replace(NUMSTRINGS[y], str(y))
+                loopbreak = 1
+                break
+    return line
 
 def main():
     input_file = open(INPUT, 'r')
@@ -20,6 +50,8 @@ def main():
     values = []
 
     for line in Lines:
+        line = numstring_to_digit(line.strip())
+
         newline = re.split('[a-zA-Z]', line.strip())
         newline = remove_item(newline, '')
         print (newline)
