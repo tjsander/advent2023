69197a34157bf09670b8623676be3feeb5e97883
Travis Sanders <tjsander@gmail.com>
2023-12-19 11:29:27 -0800

    Day19 pt 2 still something off in calculating these ranges

--- day19/day19.py
+++ day19/day19.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import re
+import numpy
 
 INPUT = 'day19/test_input.txt'
 # INPUT = 'day19/input.txt'
@@ -59,20 +60,22 @@ def process_ranges(range_set, work_id, workflows):
             result = flow.split(":")[1]
             if result == "A":
                 list_ranges.extend([pass_range])
-                return list_ranges
-            if result == "R":
-                return list_ranges
-            if (len(pass_range) > 0):
-                list_ranges.extend(process_ranges(pass_range, result, workflows))
-                return list_ranges
+
+            elif result == "R":
+
+                if (len(pass_range) > 0):
+                    range_set = pass_range
+            else:
+                if (len(pass_range) > 0):
+                    list_ranges.extend(process_ranges(pass_range, result, workflows))
+
+                if (len(fail_range) > 0):
+                    range_set = fail_range
         else:
             if flow == "A":
-                list_ranges.extend(range_set)
-                return list_ranges
-            if flow == "R":
-                return list_ranges
-            list_ranges.extend(process_ranges(range_set, flow, workflows))
-            return list_ranges
+                list_ranges.extend([range_set])
+            elif flow != "R":
+                list_ranges.extend(process_ranges(range_set, flow, workflows))
     return list_ranges
 
 def parse_eval_range(range_eval, flow):
@@ -84,17 +87,28 @@ def parse_eval_range(range_eval, flow):
     else:
          split_f = flow.split(":")[0].split(">")
 
+    character = split_f[0]
     comparison_int = int(split_f[1])
 
     top_range    = range_eval.copy()
-    top_range[split_f[0]] = top_range[split_f[0]][:comparison_int]
-
     bottom_range = range_eval.copy()
-    bottom_range[split_f[0]] = bottom_range[split_f[0]][comparison_int:]
 
-    if len(bottom_range[split_f[0]]) == 0:
+    lower_range = top_range[split_f[0]][0]
+
+    if (lower_range > 0):
+        comparison_int -= lower_range
+    ### PROBLEM HERE: Slicing was not occuring at the number specified,
+    # It is happening at that INDEX. ie - 2770 is out of range 1351:4000
+
+    if comparison_int > 0:
+        top_range[split_f[0]] = top_range[character][:comparison_int]
+        bottom_range[character] = bottom_range[character][comparison_int:]
+    else:
+        print("ERROR")
+
+    if len(bottom_range[character]) == 0:
         bottom_range = []
-    if len(top_range[split_f[0]]) == 0:
+    if len(top_range[character]) == 0:
         top_range = []
 
     if (less_than):
@@ -102,6 +116,14 @@ def parse_eval_range(range_eval, flow):
     else:
         return (bottom_range, top_range)
 
+def get_overlapping_range_total(ranges_a, ranges_b):
+    totals = []
+    keys = ["x","m","a","s"]
+    # for key in ranges_a.items():
+    for key in keys:
+        total = len(set(ranges_a[key]) & set(ranges_b[key]))
+        totals.append(total)
+    return numpy.prod(totals)
 
 def main():
     input_file = open(INPUT, 'r')
@@ -136,10 +158,6 @@ def main():
 
     ranges = {"x":range(0,4000),"m":range(0,4000),"a":range(0,4000),"s":range(0,4000)}
     range_results = []
-    # range_1 = ranges["x"][2000:]
-    # range_2 = ranges["x"][:2000]
-    # ranges["x"] = range_2
-    # range_2 = range_2[:50]
     range_results = process_ranges(ranges,"in", workflows)
     print (range_results)
 
@@ -149,7 +167,32 @@ def main():
         for key, value in result.items():
             range_total = range_total * len(value)
         range_totals.append(range_total)
+
+    #NEED TO FIND A WAY TO REMOVE DUPES....
     print(sum(range_totals))
+
+    dupe_totals = []
+    for i in range(0,len(range_results)):
+        for j in range(i+1,len(range_results)):
+            total = get_overlapping_range_total(range_results[i],range_results[j])
+            dupe_totals.append(total)
+            # print("I={} J={}".format(i,j))
+    print(sum(dupe_totals))
+
+    print(sum(range_totals) - sum(dupe_totals))
+
+    # total = 0
+    # for x in range (0,4000):
+    #     print("ex")
+    #     for m in range (0,4000):
+    #         print("em")
+    #         for a in range (0,4000):
+    #             for s in range (0,4000):
+    #                 for result in range_results:
+    #                     if (x in result["x"] and m in result["m"] and a in result["a"] and s in result["s"]):
+    #                         total +=1
+    # print(total)
+
 
 
 if __name__ == '__main__':
